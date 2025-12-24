8c763dc61b30568450d1f690d46788620543f018
afs-eng <andrealekhine@gmail.com>
2025-12-24 11:30:10 -0300

    Atualizando as tabelas NCP.

--- neuro/wisc/testes-wisc.py
+++ neuro/wisc/testes-wisc.py
@@ -5,7 +5,12 @@ import pandas as pd
 #df = pd.read_excel(TABELAS_A8 / "Tabela_A8_4_idades_10_0a10_11.xlsx", engine="openpyxl")
 
 # arquivo csv
-df = pd.read_csv(TABELAS_NCP / 'tabela_a1.csv')
+df = pd.read_csv(TABELAS_NCP / 'idade_6-4-6-7.csv')
+
+
+
+df.rename(columns={'Pontos Ponderado': 'PB'}, inplace=True )
+
+df.to_csv('idade_6-4-6-7.csv', index=False)
 
 print(df.head())
-
